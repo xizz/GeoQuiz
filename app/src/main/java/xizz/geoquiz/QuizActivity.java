6c24750b7daafcc01fda90162a3517a51f9bb2b5
Xi Wei <xwei@amplify.com>
2015-04-30 17:55:37 -0400

    Chapter 2 challenge completed & code refactored

--- app/src/main/java/xizz/geoquiz/QuizActivity.java
+++ app/src/main/java/xizz/geoquiz/QuizActivity.java
@@ -11,9 +11,7 @@ import android.widget.Toast;
 
 public class QuizActivity extends Activity {
 
-	private Button mTrueButton;
-	private Button mFalseButton;
-	private Button mNextButton;
+	private Button mPreviousButton;
 
 	private TextView mQuestionTextView;
 
@@ -32,34 +30,16 @@ public class QuizActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
 
+		mPreviousButton = (Button) findViewById(R.id.previous_button);
 		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
-
-		mTrueButton = (Button) findViewById(R.id.true_button);
-		mTrueButton.setOnClickListener(new View.OnClickListener() {
+		mQuestionTextView.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				checkAnswer(true);
+				click(v);
 			}
 		});
 
-		mFalseButton = (Button) findViewById(R.id.false_button);
-		mFalseButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				checkAnswer(false);
-			}
-		});
-
-		mNextButton = (Button) findViewById(R.id.next_button);
-		mNextButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				mCurrentIndex = (mCurrentIndex + 1) % mAnswerKey.length;
-				updateQuestion();
-			}
-		});
-
-		updateQuestion();
+		updateUI();
 	}
 
 	@Override
@@ -68,17 +48,44 @@ public class QuizActivity extends Activity {
 		return true;
 	}
 
-	private void updateQuestion() {
-		int question = mAnswerKey[mCurrentIndex].question;
+	private void updateUI() {
+		int question = mAnswerKey[mCurrentIndex % mAnswerKey.length].question;
 		mQuestionTextView.setText(question);
+
+		if (mCurrentIndex > 0)
+			mPreviousButton.setEnabled(true);
+		else
+			mPreviousButton.setEnabled(false);
 	}
 
 	private void checkAnswer(boolean userAnswer) {
-		boolean answer = mAnswerKey[mCurrentIndex].answer;
+		boolean answer = mAnswerKey[mCurrentIndex % mAnswerKey.length].answer;
 
 		if (userAnswer == answer)
 			Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
 		else
 			Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+	}
+
+	public void click(View v) {
+		switch (v.getId()) {
+			case R.id.true_button:
+				checkAnswer(true);
+				break;
+			case R.id.false_button:
+				checkAnswer(false);
+				break;
+			case R.id.previous_button:
+				if (mCurrentIndex > 0) {
+					--mCurrentIndex;
+					updateUI();
+				}
+				break;
+			case R.id.question_text_view:
+			case R.id.next_button:
+				++mCurrentIndex;
+				updateUI();
+				break;
+		}
 	}
 }
